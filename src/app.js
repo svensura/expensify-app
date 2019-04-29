2081fba2cd9fcc67d7738c8faaa246a521e794a8
ssura <svensura@germanscreens.de>
2019-04-29 13:44:45 +0200

    Remove dummy expenses

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore'
-import getVisibleExpenses from './selectors/expenses'
-import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css'
@@ -13,17 +10,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
-
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500,  }));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500,  }));
-
-
-
-//console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters))
-
-//console.log(store.getState())
 
 const jsx = (
     <Provider store={store}>
