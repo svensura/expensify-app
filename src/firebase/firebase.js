c8853aec74696c2024dcefad1daa403e2fff3d9e
ssura <svensura@germanscreens.de>
2019-05-02 18:12:14 +0200

    Firebase Bucket Error solved

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,7 +6,7 @@ const config = {
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
