8cceabcab71c1199357b8a75b2caca1f5170fb02
ssura <svensura@germanscreens.de>
2019-05-17 16:51:54 +0200

    store user data in user area

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensedPage from '../components/AddExpensePage';
 import EditExpensedPage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 
 export const history = createHistory()
@@ -17,11 +17,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={LoginPage} exact={true} />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
                 <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
                 <PrivateRoute path="/create" component={AddExpensedPage} />
                 <PrivateRoute path="/edit/:id" component={EditExpensedPage} />
-                <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>  
